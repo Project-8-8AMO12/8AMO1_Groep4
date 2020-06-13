3e5b96d8d5493ffbe315d6d85985c9692f057833
mitchell.n@outlook.com <Llechtim19961>
2020-06-13 15:08:26 +0200

    Home nav - Start met admin panel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
-        <title>@yield('title','Imkers')</title>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <meta property="og:locale" content="en_US">
+        <meta property="og:url" content="{{ request()->url() }}">
+        <meta property="og:title" content="@yield('title')">
+        <meta property="og:image" content="@yield('image')">
+        <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet" type="text/css" >
+        <script src="{{asset(mix('js/app.js')) }}"></script>
     </head>
     <body>
-
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
-
-{{--            <h1 class="logo"><a href="index.html">Kelly</a></h1>--}}
-            <!-- Uncomment below if you prefer to use an image logo -->
-           <a href="index.html" class="logo"><img src="{{ asset('images/logo.png') }}" alt="" class="img-fluid"></a>
-
-            <nav class="nav-menu d-none d-lg-block">
-                <ul>
-                    <li class="active"><a href="{{ url('/') }}">Home</a></li>
-                    <li><a href="{{ url('/') }}">About</a></li>
-                    <li><a href="{{ url('/') }}">Resume</a></li>
-                    <li><a href="{{ url('/') }}">Services</a></li>
-                    <li><a href="{{ url('/') }}">Portfolio</a></li>
-                    <li><a href="{{ url('/') }}">Contact</a></li>
-                </ul>
-            </nav><!-- .nav-menu -->
-
-            <div class="header-social-links">
-                <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-                <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-                <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
-            </div>
-
-        </div>
-    </header><!-- End Header -->
+    @include('nav.nav')
     </body>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
