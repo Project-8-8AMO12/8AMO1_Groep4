c01837e5f275123703ff266feb942de4ae171e75
mitchell.n@outlook.com <Llechtim19961>
2020-06-21 11:36:04 +0200

    Login en welcome klaar

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,26 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('/activiteiten', 'ActiviteitenController@index');
 
-Route::get('/admin', function () {
-    return view('admin');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+Route::get('/users/forgotpassword', 'Auth\LoginController@userLogout')->name('user.forgot');
 
 Route::get('/nieuws', 'NieuwsController@index');
 Route::post('Cursussen', 'ActiviteitenController@index');
 Route::get('/Bijenstal','BijenstalController@index');
+
+Route::prefix('admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+    // Password reset routes
+    Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+    Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+});
 
