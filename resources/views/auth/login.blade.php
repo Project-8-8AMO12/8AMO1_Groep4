765be54add587ba17faa1093dce5985505d40f1f
mitchell.n@outlook.com <Llechtim19961>
2020-06-21 12:43:41 +0200

    Activiteiten pagina gebruikt sections nu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="card bg-light">
         <article class="card-body mx-auto" style="max-width: 400px;">
-            <h4 class="card-title mt-3 text-center">Account aanmaken</h4>
+            <h4 class="card-title mt-3 text-center">Login</h4>
             <form class="form-box" role="form" method="POST" action="{{ route('register') }}">
                 {{ csrf_field() }}
                 <div class="form-group">
@@ -23,7 +23,7 @@
                     @endif
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block"> Account aanmaken </button>
+                    <button type="submit" class="btn btn-primary btn-block"> Login </button>
                 </div> <!-- form-group// -->
                 <p class="text-center">Wachtwoord Vergeten? <a href="reset.blade.php">Vraag aan</a> </p>
             </form>
