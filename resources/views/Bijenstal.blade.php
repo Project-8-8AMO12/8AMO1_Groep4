0b7854bfe9bf0e609a29391a2dc45c0d15ee0fbe
Nick van Slooten <nickfloor11@gmail.com>
2020-06-25 11:24:26 +0200

    Bijenstal Pagina

---
+++ resources/views/Bijenstal.blade.php
@@ -0,0 +1,27 @@
+@extends('nav.nav')
+
+
+
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title m-b-md" style="text-align: center; padding-top: 8vh; color: #9fb23a; ">
+                <h1>Bijenstal Pagina</h1>
+            </div>
+
+            <div class="row" style="padding-top: 5vh; padding-bottom: 5vh;">
+                <div class="column" style="padding-left: 1vw;"><blockquote>
+                        @foreach($text as $texts)
+                            <h1 style="color: #9fb23a">{{ $texts->Titel }}</h1> <h2 style="color: #ffd73d">{{ $texts->SubTitel }}</h2> <div>{{ $texts->Tekst }}</div>
+                        @endforeach
+
+                    </blockquote></div>
+                <div class="column">
+<img class="img-fluid" style="max-width: 100%;" src="{{asset('images/image002.png')}}">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+@endsection
